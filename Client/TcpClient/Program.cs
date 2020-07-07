4bab12a1f7f6142d58c910e6eecf984bdfe0e767
Helgezec <54369812+Helgezec@users.noreply.github.com>
2020-07-07 19:20:26 +0500

    Update Program.cs

--- Client/TcpClient/Program.cs
+++ Client/TcpClient/Program.cs
@@ -7,11 +7,10 @@ namespace TcpClient
     {
         static void Main(string[] args)
         {
-            args = new[] {"127.0.0.1", "8080"};
             var client = new System.Net.Sockets.TcpClient(args[0], int.Parse(args[1]));
             var stream = client.GetStream();
 
-            Console.WriteLine("Введите команду вида: get <prefix>");
+            Console.WriteLine("Enter a command of the form: get <prefix>");
             while (true)
             {
                 var data = Encoding.Unicode.GetBytes(Console.ReadLine());
